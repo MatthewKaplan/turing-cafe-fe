ac91d80de39017b7b8e80cb982992fe9cb9d5d8f
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-16 09:53:18 -0600

    Add proper input fields and display them to the DOM

--- src/ButtonContainer.js
+++ src/ButtonContainer.js
@@ -8,6 +8,15 @@ class ButtonContainer extends Component {
   render() {
     return (
       <div className="buttonContainer">
+        <input type="text" className="name" placeholder="Name" />
+        <input type="text" className="date" placeholder="Date (mm/dd)" />
+        <input type="text" className="time" placeholder="Time" />
+        <input
+          type="text"
+          className="number-of-guests"
+          placeholder="Number of guests"
+        />
+        <button className="make-reservation">Make Reservation</button>
       </div>
     );
   }
