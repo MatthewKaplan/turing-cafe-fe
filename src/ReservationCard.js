030fd450fccfccdb2c540c807ce27171edd2ddad
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-16 10:11:15 -0600

    Set up reservation card with correct information

--- src/ReservationCard.js
+++ src/ReservationCard.js
@@ -5,7 +5,11 @@ class ReservationCard extends Component {
   render() {
     return (
       <div className="reservation-card">
-
+        <h2 className="reservation-name">Name...</h2>
+        <p className="reservation-date">Date...</p>
+        <p className="reservation-time">Time...</p>
+        <p className="reservation-guests">Number of guests: </p>
+        <button>Cancel</button>
       </div>
     );
   }
--- src/ReservationContainer.js
+++ src/ReservationContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class ReservationContainer extends Component{
-  constructor() {
+  constructor(props) {
     super()
 
   }
